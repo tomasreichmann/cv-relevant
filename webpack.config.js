a77eb22bd25686520b15553946332854c34ea7b5
Tomáš Reichmann <tomas.reichmann@sinnerschrader.com>
2017-02-07 13:51:10 +0100

    build update to support multiple html outputs, subpage templates and hp content

--- webpack.config.js
+++ webpack.config.js
@@ -5,67 +5,79 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var dirApp = path.join(__dirname, 'app');
 var dirAssets = path.join(__dirname, 'assets');
 
+const pages = [
+  {
+    template: 'templates/index.handlebars',
+    filename: 'index.html'
+  },
+  {
+    template: 'templates/contracting/index.handlebars',
+    filename: 'contracting/index.html'
+  },
+  {
+    template: 'templates/headhunting/index.handlebars',
+    filename: 'headhunting/index.html'
+  },
+  {
+    template: 'templates/obsazene-pozice/index.handlebars',
+    filename: 'obsazene-pozice/index.html'
+  },
+  {
+    template: 'templates/reference/index.handlebars',
+    filename: 'reference/index.html'
+  },
+
+  // English
+  {
+    template: 'templates/en/index.handlebars',
+    filename: 'en/index.html'
+  }
+];
+
 module.exports = {
-    entry: path.join(dirApp, 'index'),
-    resolve: {
-        modulesDirectories: [
-            'node_modules'
-        ],
-        root: [
-            dirApp,
-            dirAssets
-        ]
-    },
-    plugins: [
-        new webpack.optimize.DedupePlugin(),
-
-        new webpack.optimize.OccurenceOrderPlugin(true),
-
-        new webpack.ProvidePlugin({
-            // jQuery
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            'root.jQuery': 'jquery',
-
-            // lodash
-            '_': 'lodash'
-        }),
-
-        new webpack.optimize.CommonsChunkPlugin('vendor', '[name].[hash].js'),
-
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.handlebars'),
-            output: {
-                path: 'dist',
-                filename: 'index.html'
-            },
-            inject: true
-        })
+  entry: path.join(dirApp, 'index'),
+  resolve: {
+    modulesDirectories: [
+      'node_modules'
     ],
-    module: {
-        loaders: [
-            // Babel loader
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015'],
-                    compact: true
-                }
-            },
+    root: [
+      dirApp,
+      dirAssets
+    ]
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+  ].concat(pages.map( (page) => (
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, page.template),
+      filename: page.filename
+    })
+  ) ) ),
+  module: {
+    loaders: [
+      // Babel loader
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          compact: true
+        }
+      },
 
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
 
-            // Handlebars
-            { test: /\.handlebars$/, loader: 'handlebars-loader' },
+      // Handlebars
+      { test: /\.handlebars$/, loader: 'handlebars-loader' },
 
-            // Image loader
-            { test: /\.(jpe?g|png|gif)$/, loader: 'file?name=assets/images/[hash].[ext]' }
-        ]
-    },
-    sassLoader: {
-        includePaths: [dirAssets]
-    }
+      // Image loader
+      { test: /\.(jpe?g|png|gif)$/, loader: 'file?name=assets/images/[hash].[ext]' }
+    ]
+  },
+  sassLoader: {
+    includePaths: [dirAssets]
+  }
 };
